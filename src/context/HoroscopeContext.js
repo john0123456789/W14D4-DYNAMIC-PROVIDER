acb9c9766693be37fb5ae7a1ce9cc250100f31dd
john0123456789 <johnluongpham@yahoo.com>
2022-06-16 09:57:15 -0700

    updated import/export

--- src/context/HoroscopeContext.js
+++ src/context/HoroscopeContext.js
@@ -1,3 +1,17 @@
-import { createContext } from 'react';
+import { useState, createContext } from "react";
+import horoscopeObj from "../data/horoscopes";
 
 export const HoroscopeContext = createContext();
+
+const HoroscopeProvider = (props) => {
+  const [currentSign, setCurrentSign] = useState("Aries");
+  const sign = horoscopeObj[currentSign];
+
+  return (
+    <HoroscopeContext.Provider value={{ sign, setCurrentSign }}>
+      {props.children}
+    </HoroscopeContext.Provider>
+  );
+};
+
+export default HoroscopeProvider;
